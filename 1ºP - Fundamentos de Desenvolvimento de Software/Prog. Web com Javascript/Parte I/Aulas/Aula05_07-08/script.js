612f4836a37453d0b998fb9b8fd9b9ed63d2651e
codinGloria <liviafariabraz282@gmail.com>
2024-08-08 11:36:41 -0300

    :white_check_mark: Aula 05

---
+++ 1ºP - Fundamentos de Desenvolvimento de Software/Prog. Web com Javascript/Parte I/Aulas/Aula05_07-08/script.js
@@ -0,0 +1,29 @@
+// Comentário de uma linha só
+let nome = 'Glória';
+
+/* Comentário
+com
+várias
+linhas 
+*/
+alert("Olá :)")
+
+// recebendo números
+let idade = Number(prompt("Digite sua idade: "));
+idade += 1;
+
+console.log(idade);
+
+//pós implemento e decremento
+idade++;
+console.log(idade);
+
+idade--;
+console.log(idade);
+
+//pré implemento e decremento
+++idade;
+console.log(idade);
+
+--idade;
+console.log(idade);
