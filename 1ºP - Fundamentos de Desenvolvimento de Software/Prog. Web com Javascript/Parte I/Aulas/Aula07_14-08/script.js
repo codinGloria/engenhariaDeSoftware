67466316ac1b09a546cc9bf0f6dcc7a8913cb186
codinGloria <liviafariabraz282@gmail.com>
2024-08-19 19:55:13 -0300

    :books: Aula 07

---
+++ 1ºP - Fundamentos de Desenvolvimento de Software/Prog. Web com Javascript/Parte I/Aulas/Aula07_14-08/script.js
@@ -0,0 +1,16 @@
+// declarando variáveis
+
+/* A variável let pode ter o valor alterado */
+let idade = 22;
+idade = 'Vinte e dois'
+console.log(idade);
+
+/* Já a variável const não pode */
+const idade2 = 22;
+console.log(idade2);
+
+// Convenções de Nomenclatura
+let nomeCompleto // camelCase
+let nome_completo // snake_case
+let NomeCompleto // PascalCase
+// let nome-completo // kebab-case'
