70a8743da1aa614475270405f26f95dbdaa97711
Glória Braz <codingloria@gmail.com>
2025-02-06 19:11:29 -0300

    📚 Aula 02

---
+++ 2ºP - Desenvolvimento Back-End/Desenvolvimento com C#/Aulas/aula02_31-01/SegundaAula/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SegundaAula;
+
+class Program {
+    static void Main(string[] args) {
+        // O C# é uma linguagem fortemente tipada
+        // O que significa que você precisa declarar o tipo de uma variável, e que NÃO pode ser alterado depois
+        
+        // Declaração de variáveis
+        int quantidade, idade = 22;
+        double preco = 2.40;
+        string nome = "Glória";
+        char letra = 'a';
+        bool achou = false;
+
+        quantidade = 28;
+        
+        Console.WriteLine("Quantidade = " + quantidade);
+        Console.WriteLine("Idade = " + idade);
+        Console.WriteLine("Preçõ = " + preco);
+        Console.WriteLine("Nome = " + nome);
+        Console.WriteLine(letra);
+        Console.WriteLine("Achou? = " + achou);
+
+    }
+}
---
+++ 2ºP - Desenvolvimento Back-End/Desenvolvimento com C#/Aulas/aula02_31-01/SegundoPrograma/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace SegundoPrograma;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Hello, World!");
+    }
+}
