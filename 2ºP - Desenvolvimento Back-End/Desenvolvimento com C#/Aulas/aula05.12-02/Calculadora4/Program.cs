6d71b77ea3a8694389c1a40f5b2764f20526860d
Glória Braz <codingloria@gmail.com>
2025-02-12 20:31:57 -0300

    📚 Class 05: understanding loops

---
+++ 2ºP - Desenvolvimento Back-End/Desenvolvimento com C#/Aulas/aula05.12-02/Calculadora4/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace aula05._12_02;
+
+class Program {
+    static void Main(string[] args) {
+
+        int opcao = EntrarOperador();
+        double op1 = EntrarOperando();
+        double op2 = EntrarOperando();
+
+        Calcular(opcao, op1, op2);
+    }
+
+    public static double EntrarOperando() {
+        Console.WriteLine("Entre com o operando: ");
+        double op = double.Parse(Console.ReadLine());
+        return op;
+    }
+
+    public static int EntrarOperador() {
+        Console.WriteLine("********** Calculadora **********");
+        Console.WriteLine("[1] Soma");
+        Console.WriteLine("[2] Subtração");
+        Console.WriteLine("[3] Multiplicação");
+        Console.WriteLine("[4] Divisão");
+        Console.WriteLine("Qual operação deseja fazer? ");
+
+        int opcao = int.Parse(Console.ReadLine());
+        return opcao;
+    }
+
+    public static void Calcular(int opcao, double op1, double op2) {
+
+        switch (opcao) {
+            case 1:
+                Somar(op1, op2);
+                break;
+            case 2:
+                Subtrair(op1, op2);
+                break;
+            case 3:
+                Multiplicar(op1, op2);
+                break;
+            case 4:
+                Dividir(op1, op2);
+                break;
+            default:
+                Console.WriteLine("Opção inválida");
+                break;
+        }
+    }
+
+    public static void Somar(double a, double b) {
+        double result = a + b;
+        Console.WriteLine("Soma = " + result);
+    }
+
+    public static void Subtrair(double a, double b) {
+        double result = a - b;
+        Console.WriteLine("Subtração = " + result);
+    }
+
+    public static void Multiplicar(double a, double b) {
+        double result = a * b;
+        Console.WriteLine("Multiplicação = " + result);
+    }
+
+    public static void Dividir(double a, double b) {
+        if (b != 0) {
+            double result = a / b;
+            Console.WriteLine("Divisão = " + result);
+        }
+        else {
+            Console.WriteLine("Erro: Divisão por zero não é permitida");
+        }
+    }
+
+}
---
+++ 2ºP - Desenvolvimento Back-End/Desenvolvimento com C#/Aulas/aula05.12-02/ComandosRepeticao/ForAndWhile.cs
@@ -0,0 +1,31 @@
+﻿namespace ComandosRepeticao;
+
+class ForAndWhile {
+    static void Main(string[] args) {
+        /* For ir used when we know the number of iterations
+         * 
+         * Initialization of the control variable i
+         * Exit condition
+         * Increment the control variable
+        */
+
+        const int N = 10;
+        int sum = 0;
+        int sum2 = 0;
+        
+        
+        for(int i = 0; i < N; i++) {
+            sum += i;
+        }
+        Console.WriteLine("Sum = " + sum);
+        
+        // Same code using while:
+        int j = 0;
+        while (j <= N) {
+            sum2 += j;
+            j++;
+        }
+        Console.WriteLine("Sum = " + sum2);
+        
+    }
+}
