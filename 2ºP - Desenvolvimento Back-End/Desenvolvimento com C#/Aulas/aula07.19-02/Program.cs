941749d86b2557a757b5e968d1408f6d2923ff4e
Glória Braz <codingloria@gmail.com>
2025-02-25 10:39:06 -0300

    📚 Class 07: introducing OOP

---
+++ 2ºP - Desenvolvimento Back-End/Desenvolvimento com C#/Aulas/aula07.19-02/Program.cs
@@ -0,0 +1,28 @@
+﻿using OOPv1.models;
+
+namespace OOPv1 {
+    internal class Program {
+        static void Main(string[] args) {
+            Student student3 = new Student("Neide Fonseca", 6, 8); // This is the instantiation of the Student class
+            Student student1 = new Student();
+            student1.SetName("Glória Braz");
+            student1.SetGrade1(8);
+            student1.SetGrade2(10);
+            Console.WriteLine(student1.GetName() + ": " + student1.GetGrade1() + ", " + student1.GetGrade2());
+            Console.WriteLine("Average = " + student1.CalculateAverage());
+            Console.WriteLine();
+
+            Student student2 = new Student();
+            student2.SetName("Luan Felipe");
+            student2.SetGrade1(8);
+            student2.SetGrade2(7);
+            Console.WriteLine(student2.GetName() + ": " + student2.GetGrade1() + ", " + student2.GetGrade2());
+            Console.WriteLine("Average = " + student2.CalculateAverage());
+            Console.WriteLine();
+
+            Console.WriteLine(student3.GetName() + ": " + student3.GetGrade1() + ", " + student3.GetGrade2());
+            Console.WriteLine("Average = " + student3.CalculateAverage());
+            
+        }
+    }
+}
---
+++ 2ºP - Desenvolvimento Back-End/Desenvolvimento com C#/Aulas/aula07.19-02/models/Student.cs
@@ -0,0 +1,60 @@
+﻿namespace OOPv1.models;
+
+class Student {
+    // Attributes or properties
+    private string name;
+    private double grade1;
+    private double grade2;
+
+    // Constructor
+    public Student(){}
+    
+    public Student(string name, double grade1, double grade2) {
+        this.name = name;
+        this.grade1 = grade1;
+        this.grade2 = grade2;
+    }
+    
+    // Methods
+    // Getters & Setters
+    public void SetName(string name) {
+        if (name.Length == 0) {
+            return;
+        }
+        this.name = name;
+    }
+    
+    public String GetName() {
+        string[] nameLastName = name.Split(" ");
+        string myName = nameLastName[0];
+        string myLastName = nameLastName[1];
+        return myLastName + ", " + myName;
+    }
+
+    public void SetGrade1(double grade1) {
+        if (grade1 < 0) {
+            return;
+        }
+        this.grade1 = grade1;
+    }
+
+    public double GetGrade1() {
+        return grade1;
+    }
+    
+    public void SetGrade2(double grade2) {
+        if (grade2 < 0) {
+            return;
+        }
+        this.grade2 = grade2;
+    }
+
+    public double GetGrade2() {
+        return grade2;
+    }
+    
+    
+    public double CalculateAverage() {
+        return (grade1 + grade2) / 2;
+    }
+}
