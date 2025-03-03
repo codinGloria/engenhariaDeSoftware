2013a63129bfdd31eae919a33704af02c85321df
Glória Braz <codingloria@gmail.com>
2025-03-03 16:51:22 -0300

    📚 Class 09: 26/02

---
+++ 2ºP - Desenvolvimento Back-End/Desenvolvimento com C#/Aulas/aula09.26-02/Calculator07/Models/Calculates.cs
@@ -0,0 +1,29 @@
+﻿namespace Calculator07.Models;
+
+class Calculates {
+    public double Op1 { get; set; }
+    public double Op2 { get; set; }
+    
+    public Calculates(){}
+    
+    public Calculates(double op1, double op2) {
+        Op1 = op1;
+        Op2 = op2;
+    }
+    
+    public double Add(double op1, double op2) {
+        return op1 + op2;
+    }
+    
+    public double Subtract(double op1, double op2) {
+        return op1 - op2;
+    }
+    
+    public double Multiply(double op1, double op2) {
+        return op1 * op2;
+    }
+    
+    public double Divide(double op1, double op2) {
+        return op1 / op2;
+    }
+}
---
+++ 2ºP - Desenvolvimento Back-End/Desenvolvimento com C#/Aulas/aula09.26-02/Calculator07/Program.cs
@@ -0,0 +1,116 @@
+﻿using Calculator07.Models;
+
+namespace Calculator07;
+
+class Program {
+    static void Main(string[] args) {
+        const int EXIT = 0;
+        double op1, op2;
+        int option = EnterOption();
+
+        while (option != EXIT) {
+            op1 = EnterOp();
+            op2 = EnterOp();
+            
+            Calculate(option, op1, op2);
+            
+            option = EnterOption();
+        }
+
+        Console.WriteLine("Out of the loop!");
+    }
+    
+    public static void Calculate(int option, double op1, double op2) {
+        Calculates calc = new Calculates(op1, op2);
+        double result;
+        
+        switch (option) {
+            case 1:
+                result = calc.Add(op1, op2);
+                Console.WriteLine("Sum: " + result);
+                break;
+            case 2:
+                result = calc.Subtract(op1, op2);
+                Console.WriteLine("Subtraction: " + result);
+                break;
+            case 3:
+                result = calc.Multiply(op1, op2);
+                Console.WriteLine("Multiplication: " + result);
+                break;
+            case 4:
+                if (op2 != 0) {
+                    result = calc.Divide(op1, op2);
+                    Console.WriteLine("Division: " + result);
+                } else {
+                    Console.WriteLine("Division by zero is not allowed!");
+                }
+                break;
+            default: 
+                Console.WriteLine("Invalid option!"); 
+                break;
+        }
+    }
+
+    public static void Menu() {
+        Console.WriteLine("\n########## Calculator ##########");
+        Console.WriteLine("[1] Add");
+        Console.WriteLine("[2] Subtract");
+        Console.WriteLine("[3] Multiply");
+        Console.WriteLine("[4] Divide");
+        Console.WriteLine("[0] Exit");
+        Console.WriteLine("Enter the option number: ");
+    }
+    
+    public static bool ValidateOption(int option) {
+        return (option >= 0) && (option <= 4);
+    }
+    
+    public static int EnterInt() {
+        int option;
+        
+        do {
+            try {
+                option = int.Parse(Console.ReadLine());
+                break;
+            }
+            catch (FormatException) {
+                Console.WriteLine("Invalid value! Try again.");
+                Menu();
+            }
+        } while (true);
+        
+        return option;
+    }
+    
+    public static int EnterOption() {
+        int option;
+        
+        do {
+            Menu();
+            option = EnterInt();
+            if (ValidateOption(option)) {
+                break;
+            } else {
+                Console.WriteLine("Invalid option!");
+            }
+        } while (true);
+
+        return option;
+    }
+    
+    public static double EnterOp() {
+        double op;
+
+        do {
+            try {
+                Console.Write("Enter a number: ");
+                op = double.Parse(Console.ReadLine());
+                break;
+            }
+            catch (FormatException) {
+                Console.WriteLine("Invalid number! Try again.");
+            }
+        } while (true);
+        return op;
+    }
+}
