9e22ce61561f4ac3e5ab1d45252d68ed91ac31ff
Glória Braz <codingloria@gmail.com>
2025-03-06 16:44:34 -0300

    📚 Class 10

---
+++ 2ºP - Desenvolvimento Back-End/Desenvolvimento com C#/Aulas/aula10.28-02/exception/exception01/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace exception01;
+
+class Program {
+    static void Main(string[] args) {
+        int num = 0;
+
+        do {
+            try {
+                Console.Write("Enter a number: ");
+                num = int.Parse(Console.ReadLine());
+                break;
+            }
+            catch (FormatException) {
+                Console.WriteLine("Error: invalid number.");
+            }
+        } while (true);
+
+        Console.WriteLine(num);
+    }
+}
---
+++ 2ºP - Desenvolvimento Back-End/Desenvolvimento com C#/Aulas/aula10.28-02/exception/exception02/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace exception02;
+
+class Program {
+    static void Main(string[] args) {
+        int op1 = 4, op2 = 0;
+        double result;
+
+        try {
+            result = op1 / op2;
+            Console.WriteLine("Result: " + result);
+        }
+        catch (DivideByZeroException) {
+            Console.WriteLine("Error: division by zero.");
+        }
+    }
+}
