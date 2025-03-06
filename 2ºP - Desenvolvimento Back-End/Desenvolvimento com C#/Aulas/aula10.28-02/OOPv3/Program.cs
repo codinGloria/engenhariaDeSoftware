682b9afae219bad9a20884faef29d051d2163c39
Glória Braz <codingloria@gmail.com>
2025-03-06 16:43:42 -0300

    📚 Class 10

---
+++ 2ºP - Desenvolvimento Back-End/Desenvolvimento com C#/Aulas/aula10.28-02/OOPv3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace OOPv3;
+
+class Program {
+    static void Main(string[] args) {
+        
+        string[] names = {"Zoro", "Nami", "Robin", "Luffy"};
+        double[] grades1 = {7, 8, 10, 6};
+        double[] grades2 = {6, 7, 10, 4};
+        double average;
+        
+        for (int i = 0; i < names.Length; i++) {
+            Console.WriteLine(names[i] + ": " + grades1[i] + " - " + grades2[i]);
+        }
+
+        for (int i = 0; i < names.Length; i++) {
+            average = (grades1[i] + grades2[i]) / 2;
+            Console.WriteLine("--------------------");
+            if (average >= 6) {
+                Console.WriteLine(names[i] + " approved! Average: " + average);
+            } else {
+                Console.WriteLine(names[i] + " reproved! Average: " + average);
+            }
+        }
+    }
+}
---
+++ 2ºP - Desenvolvimento Back-End/Desenvolvimento com C#/Aulas/aula10.28-02/OOPv4/Program.cs
@@ -0,0 +1,36 @@
+﻿using OOPv4.models;
+
+namespace OOPv4;
+
+class Program {
+    static void Main(string[] args) {
+        Student[] students = new Student[3];
+        double average;
+        
+        students = CreateStudents();
+        
+        foreach (Student student in students) {
+            Console.WriteLine(student);
+        }
+        
+        foreach (Student student in students) {
+            average = (student.Grade1 + student.Grade2) / 2;
+            Console.WriteLine("--------------------");
+            if (average >= 6) {
+                Console.WriteLine(student.Name + " approved! Average: " + average);
+            } else {
+                Console.WriteLine(student.Name + " reproved! Average: " + average);
+            }
+        }
+    }
+    
+    public static Student[] CreateStudents() {
+        Student[] students = new Student[3];
+        
+        students[0] = new Student("Zoro", 7, 6);
+        students[1] = new Student("Nami", 8, 7);
+        students[2] = new Student("Robin", 10, 10);
+        
+        return students;
+    }
+}
