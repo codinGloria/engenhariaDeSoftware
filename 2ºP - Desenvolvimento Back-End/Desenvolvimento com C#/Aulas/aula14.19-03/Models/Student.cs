2b8d3eabb3cda5660c0ae338f1135dc5eb22beb3
Glória Braz <codingloria@gmail.com>
2025-04-12 17:12:50 -0300

    📚 Class 14

---
+++ 2ºP - Desenvolvimento Back-End/Desenvolvimento com C#/Aulas/aula14.19-03/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿namespace File01.Models;
+
+public class Student {
+    public string Name { get; set; }
+    public double Grade1 { get; set; }
+    public double Grade2 { get; set; }
+
+    public Student() {
+        
+    }
+    
+    public Student(string name, double grade1, double grade2) {
+        Name = name;
+        Grade1 = grade1;
+        Grade2 = grade2;
+    }
+    
+    public override string ToString() {
+        return $"Name: {Name}, Grade 1: {Grade1}, Grade 2: {Grade2}";
+    }
+}
---
+++ 2ºP - Desenvolvimento Back-End/Desenvolvimento com C#/Aulas/aula14.19-03/Program.cs
@@ -0,0 +1,47 @@
+﻿using File01.Models;
+
+namespace File01;
+
+class Program {
+    
+    const string ARQ = "C:\\Users\\Glória\\Downloads\\File\\File01\\students.csv";
+
+    static void Main(string[] args) {
+        List<Student> students = ReadFile();
+        Console.WriteLine("Students:");
+        ExhibitStudents(students);
+    }
+
+    public static void ExhibitStudents(List<Student> students) {
+        foreach (var student in students) {
+            Console.WriteLine(student);
+        }
+    }
+    
+    public static List<Student> ReadFile() {
+        var students = new List<Student>();
+        
+        try {
+            string line;
+            // Open
+            using (var file = new StreamReader(ARQ)) {
+                line = file.ReadLine();
+                while (line != null) { // null = EOF
+                    string[] fields = line.Split(',');
+                    
+                    string name = fields[0];
+                    double grade1 = double.Parse(fields[1]);
+                    double grade2 = double.Parse(fields[2]);
+                    students.Add(new Student(name, grade1, grade2));
+                    
+                    line = file.ReadLine();
+                }
+            }
+        }
+        catch (Exception e) {
+            Console.WriteLine(e);
+        }
+
+        return students;
+    }
+}
